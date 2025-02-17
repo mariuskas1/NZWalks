716f02e1ecc3f21f1038fe9e16c53cae5817e413
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-17 14:01:42 +0100

    Added Register-Method

--- NZWalks.API/Controllers/AuthController.cs
+++ NZWalks.API/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTOs;
 
 namespace NZWalks.API.Controllers
 {
@@ -7,6 +9,41 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<IdentityUser> userManager;
 
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDTO.Username,
+                Email = registerRequestDTO.Username
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDTO.Password);
+
+            if (identityResult.Succeeded) 
+            {
+                if (registerRequestDTO.Roles != null && registerRequestDTO.Roles.Any()) 
+                {
+                   identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDTO.Roles);
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User was registered.");
+                    }
+                }
+            }
+
+            return BadRequest();
+
+        }
     }
 }
---
+++ NZWalks.API/Models/DTOs/RegisterRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTOs
+{
+    public class RegisterRequestDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string[] Roles{ get; set; }
+
+    }
+}
